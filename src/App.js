89a7bcbf83c14d9bc2d5fed1e5baec89349a3947
Kyle <djprophecy21@gmail.com>
2022-12-27 15:36:28 -0600

    pizza nav-started now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +6,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Kyle webspin
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import './App.css';
+import navbar from './Navbar.js';
+
+
+
+
+function main() {
+    return (
+        <main>
+            <navbar />
+
+        </main>
+
+    );
+}
